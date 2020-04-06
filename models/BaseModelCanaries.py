4c2deb6225c9ac0eb061a89821aa159e97497f14
Dominik Dancs <dodancs@moow.info>
2020-04-06 17:51:56 +0200

    Added check for config.json

--- models/BaseModelCanaries.py
+++ models/BaseModelCanaries.py
@@ -2,8 +2,14 @@ import peewee
 import json
 
 # Open configuration file
-with open('config.json', encoding='utf8') as config_file:
-    Config = json.load(config_file)
+try:
+    with open('config.json', encoding='utf8') as config_file:
+        Config = json.load(config_file)
+except:
+    print('Unable to open config.json!')
+    print('If you haven\'t created a configuration file, please use the example configuration file as a guide.')
+    print('Otherwise, make sure the file permissions are set correctly.')
+    exit(1)
 
 db = peewee.MySQLDatabase(
     Config['canary']['db_db'], host=Config['canary']['db_host'], port=Config['canary']['db_port'], user=Config['canary']['db_user'], passwd=Config['canary']['db_password'], charset=Config['canary']['db_charset'])
--- models/BaseModelMail.py
+++ models/BaseModelMail.py
@@ -2,8 +2,14 @@ import peewee
 import json
 
 # Open configuration file
-with open('config.json', encoding='utf8') as config_file:
-    Config = json.load(config_file)
+try:
+    with open('config.json', encoding='utf8') as config_file:
+        Config = json.load(config_file)
+except:
+    print('Unable to open config.json!')
+    print('If you haven\'t created a configuration file, please use the example configuration file as a guide.')
+    print('Otherwise, make sure the file permissions are set correctly.')
+    exit(1)
 
 db = peewee.MySQLDatabase(
     Config['mail']['db_db'], host=Config['mail']['db_host'], port=Config['mail']['db_port'], user=Config['mail']['db_user'], passwd=Config['mail']['db_password'], charset=Config['mail']['db_charset'])
