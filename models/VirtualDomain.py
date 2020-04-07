04a9e81f7582d23eeeae37d8dc11e609148577bc
Dominik Dancs <dodancs@moow.info>
2020-04-07 20:36:49 +0200

    domain names must be unique

--- models/VirtualDomain.py
+++ models/VirtualDomain.py
@@ -6,7 +6,7 @@ from models.BaseModelMail import BaseModel
 class VirtualDomain(BaseModel):
     id = peewee.BigAutoField(unique=True, index=True,
                              primary_key=True, null=False)
-    name = peewee.CharField(max_length=255, null=False)
+    name = peewee.CharField(max_length=255, null=False, unique=True)
 
     class Meta:
         table_name = 'virtual_domains'
--- schema/virtual-domains.sql
+++ schema/virtual-domains.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `virtual_domains` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
