6680d53bbd277a921df9008413868620251d0630
Dominik Dancs <dodancs@moow.info>
2020-04-07 22:34:30 +0200

    added missing except clause

--- sync.py
+++ sync.py
@@ -49,6 +49,11 @@ def setup():
     try:
         with open('config.json', encoding='utf8') as config_file:
             Config = json.load(config_file)
+    except:
+        print('Unable to open config.json!')
+        print('If you haven\'t created a configuration file, please use the example configuration file as a guide.')
+        print('Otherwise, make sure the file permissions are set correctly.')
+        exit(1)
 
     #
     # Check if the script is ran by root
